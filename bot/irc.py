c6648ae097772853580e4a6a4dbf79e9d9d22832
Colin Stevens <mail@colinjstevens.com>
2015-01-17 02:53:22 -0600

    So it turns out most of this algo was useless

--- bot/irc.py
+++ bot/irc.py
@@ -47,23 +47,13 @@ class spawnBot:
 					print(data)
 				except:
 					pass
-			if not data.endswith("\r\n"): # If the message is incomplete
-				message = data.split("\r\n")
-				for i in message[:-1]: # Loop for every element in data, except the last incomplete message
-					line = i.split(' ')
-					#print("LINE IS: ",line) ##DEBUG
-					if line[0] == "PING": # Respond to network PINGs
-						self.pong(line[1])
-				data='' # Empty buffer
-				data = message[-1] # Add the incomplete message to buffer for next loop
-			else: # Else we've received n number of complete messages
-				message = data.split("\r\n")
-				for i in message:
-					line = i.split(' ')
-					#print("ELSE LINE IS: ",line) ##DEBUG
-					if line[0] == "PING":
-						self.pong(line[1])
-					data = ''
+			message = data.split("\r\n")
+			for i in message[:-1]: # Loop for every element in data, except the last incomplete message
+				line = i.split(' ')
+				#print("LINE IS: ",line) ##DEBUG
+				if line[0] == "PING": # Respond to network PINGs
+					self.pong(line[1])
+			data = message[-1] # Add the incomplete message to buffer for next loop
 
 	def printData(self):
                 print(self.configFile.config)
