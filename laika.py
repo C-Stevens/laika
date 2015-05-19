1faaa65ec53c2e82375a58c1d1cda1019e66eda1
Colin Stevens <mail@colinjstevens.com>
2015-05-19 15:35:04 -0500

    Housekeeping

--- laika.py
+++ laika.py
@@ -8,6 +8,8 @@ if __name__ == "__main__":
 	bot_py = []
 	for i in bot_files:
 		bot_py.append(imp.load_source(os.path.splitext(os.path.basename(i))[0], i))
+	print(bot_py) ##DEBUG
+	print(bot_files) ##DEBUG
 	threads = []
 	for i in bot_py:
 		j = bot.irc.spawnBot(i)
