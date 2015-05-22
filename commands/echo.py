b4c6e315d3490658292e89ac82858b46587883bb
Colin Stevens <mail@colinjstevens.com>
2015-05-21 21:48:07 -0500

    Functional state

--- commands/echo.py
+++ commands/echo.py
@@ -1,8 +1,19 @@
-def run_check(self):
-	pass
-	
+def run_check(line_info):
+	'''Checks if the command is being called. Returns 0 if it's being called, 1 if not.'''
+	if line_info.command == config['command_str']:
+		return 0
+	else:
+		return 1
+
+def run(line_info, socket):
+	line_info.printData() ##DEBUG
+	if line_info.args:
+		message = " ".join(line_info.args)
+		socket.sendToChannel(line_info.channel, message)
+
 config = {
 	'name' : 'echo',
 	'command_str' : 'echo',
-	'auth' : False
+	'auth' : False,
+	'help' : "Repeats input back into the origin channel."
 }
