6e74e55710f687ef0a00dbfdd3d662be9dcd8ce6
Colin Stevens <mail@colinjstevens.com>
2015-05-21 21:47:18 -0500

    Update class naming convention

--- laika.py
+++ laika.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 	print(bot_files) ##DEBUG
 	threads = []
 	for i in bot_py:
-		j = bot.irc.spawnBot(i)
+		j = bot.irc.bot(i)
 		j.connect()
 		t = Thread(target=j.run)
 		threads.append(t)
