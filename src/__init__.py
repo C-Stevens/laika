b2570d0a2f02f7656345581ad1fae5e7ba71abf9
Colin Stevens <mail@colinjstevens.com>
2015-06-11 17:41:09 -0500

    Added logging utils

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ['irc', 'bot', 'command']
+__all__ = ['irc', 'bot', 'command', 'log']
 import src.bot
 import src.irc
 import src.command
+import src.log
---
+++ src/log.py
@@ -0,0 +1,36 @@
+import logging
+
+class logManager(logging.Logger):
+	def __init__(self, **kwargs):
+		logging.Logger.__init__(self, self)		
+		defaultFormat = kwargs.get('defaultFormat', '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+		if kwargs.get('critLog', True):
+			self.addStream(kwargs.get('critLogDir'), logging.CRITICAL, kwargs.get('critLogFormat', defaultFormat))
+		if kwargs.get('errLog', True):
+			self.addStream(kwargs.get('errLogDir'), logging.ERROR, kwargs.get('errLogFormat', defaultFormat))
+		if kwargs.get('warnLog', True):
+			self.addStream(kwargs.get('warnLogDir'), logging.WARNING, kwargs.get('warnLogFormat', defaultFormat))
+		if kwargs.get('infoLog', False):
+			self.addStream(kwargs.get('infoLogDir'), logging.INFO, kwargs.get('infoLogFormat', defaultFormat))
+		if kwargs.get('debugLog', False):
+			self.addStream(kwargs.get('debugLogDir'), logging.DEBUG, kwargs.get('debugLogFormat', defaultFormat))
+	def addStream(self, logDir, logLevel, logFormat):
+		if logDir is not None:
+			try:
+				handler = logging.FileHandler(logDir)
+			except FileNotFoundError as e:
+				print(e, "Defaulting message output for this stream to std_err")
+				handler = logging.StreamHandler()
+		else:
+			handler = logging.StreamHandler()
+		handler.setLevel(logLevel)
+		handler.setFormatter(logging.Formatter(logFormat))
+		handler.addFilter(levelFilter(logLevel))
+		self.addHandler(handler)
+
+class levelFilter:
+	def __init__(self, level):
+		self._level = level
+	def filter(self, logRecord):
+		return logRecord.levelno <= self._level
