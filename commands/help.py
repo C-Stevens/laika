879026996c265246e13046eb8824420e860ff892
Colin Stevens <mail@colinjstevens.com>
2015-06-21 06:17:36 -0500

    Support for optional arguments

--- commands/help.py
+++ commands/help.py
@@ -1,18 +1,15 @@
 from src.datatypes import Type
 import src.format
 
-def run(self, *args):
+def run(self, *args, **kwargs):
 	_helpPrefix = src.format.bold("help | ")
 	for i in self.parent.commandList:
 		if args[0] == i.config['command_str']:
 			self.socket.notice(self.commandData.nick, _helpPrefix+i.config['name'])
 			self.socket.notice(self.commandData.nick, _helpPrefix+i.config['help'])
-
-			_argList = list(i.config['args'])
-			for j, arg in enumerate(_argList):
-				_argList[j] = '<'+repr(arg).split(':')[0][1:]+'>'
-			_argList = ' '.join(_argList)
-			self.socket.notice(self.commandData.nick, _helpPrefix+"Usage: %s%s %s"%(self.commandData.highlightChar,i.config['command_str'],_argList))
+			self.socket.notice(self.commandData.nick, _helpPrefix+self.createUsage(i))
+			if i.config['op_args']:
+				self.socket.notice(self.commandData.nick, _helpPrefix+"Optional Arguments:\t"+self.argList(i.config['op_args']))
 			return
 	self.socket.notice(self.commandData.nick, _helpPrefix+"Help information for "+src.format.bold(args[0])+" not found.")
 	_friendlyCommandList = []
@@ -26,6 +23,7 @@ config = {
 	'name' : 'Help',
 	'command_str' : 'help',
 	'args' : (Type.msg,),
+	'op_args' : False,
 	'auth' : False,
 	'help' : "Reports help and usage information for a command."
 }
