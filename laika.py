713e10999f0dc81f106bea5c1b003c7e91e0ff05
Colin Stevens <mail@colinjstevens.com>
2015-07-08 17:15:29 -0500

    Moved work into functions, added laika config loading for logging, added argument support

--- laika.py
+++ laika.py
@@ -10,14 +10,57 @@ import yaml
 
 argParser = argparse.ArgumentParser(description="A modular chat bot for the IRC protocol.")
 argParser.add_argument("--version", action='version', version=__version__, help="Displays version information and exits")
-args = argParser.parse_args()
+argParser.add_argument("-q", "--quiet", action='store_true', help="Prevents program from outputting to terminal. Does not apply to bot, channel, socket, or server logging")
+argParser.add_argument("--config", type=argparse.FileType('r'), help="")
+argParser.add_argument("--defaultFormat", help="Sets program default logging format")
 
-rootLog = src.log.logger(infoLog=True, defaultFormat="%(message)s", debugLogFormat="[%(levelname)s] %(message)s") #TODO: Read in values from args and/or config file
+argParser.add_argument("--critLog", action='store', default=None, help="Enables or disables logging for critical messages")
+argParser.add_argument("--errLog", action='store', default=None, help="Enables or disables logging for error messages")
+argParser.add_argument("--warnLog", action='store', default=None, help="Enables or disables logging for warning messages")
+argParser.add_argument("--infoLog", action='store', default=None, help="Enables or disables logging for info messages")
+argParser.add_argument("--debugLog", action='store', default=None, help="Enables or disables logging for debug messages")
 
-if __name__ == "__main__":
+argParser.add_argument("--critLogDir", help="Sets program critical logging path")
+argParser.add_argument("--errLogDir", help="Sets program error logging path")
+argParser.add_argument("--warnLogDir", help="Sets program warning logging path")
+argParser.add_argument("--infoLogDir", help="Sets program info logging path")
+argParser.add_argument("--debugLogDir", help="Sets program debug logging path")
+
+argParser.add_argument("--critLogFormat", help="Sets program critical logging format")
+argParser.add_argument("--errLogFormat", help="Sets program error logging format")
+argParser.add_argument("--warnLogFormat", help="Sets program warning logging format")
+argParser.add_argument("--infoLogFormat", help="Sets program info logging format")
+argParser.add_argument("--debugLogFormat", help="Sets program debug logging format")
+args = vars(argParser.parse_args())
+
+def loadConfig():
+	if args['config']:
+		return yaml.load(args['config'].read(-1))
+	else:
+		return yaml.load(open(os.path.join(os.path.abspath('.'),'laika.cfg'), 'r').read(-1))
+def applyArgs(config):
+	for argVal in args:
+		if args[argVal] and argVal in ("critLog","errLog","warnLog","infoLog","debugLog"):
+			args[argVal] = validateBool(args[argVal])
+	for val in config:
+		if args[val] is not None and config[val] is not args[val]:
+			print("arg: "+repr(val)) ##DEBUG
+			print("swapping %s for %s"%(repr(config[val]),repr(args[val]))) ##DEBUG
+			config[val] = args[val]
+	if args['quiet']:
+		for i in ('critLog','errLog','warnLog','infoLog','debugLog'):
+			config[i] = False
+def validateBool(arg):
+	if arg.lower() in ("true", "t", "yes", 1):
+		return True
+	elif arg.lower() in ("false", "f", "no", 0):
+		return False
+def run():
+	config = loadConfig()
+	applyArgs(config)
+	rootLog = src.log.logger(**config)
 	rootLog.info("-------------\n"+__version__+"\n-------------")
-
-	rootLog.info("Loading bot config files..")
+	rootLog.info("Loading bot configuration files..")
 	botConfigs = [os.path.abspath(os.path.join('./config', i)) for i in os.listdir('./config') if i.endswith('.yaml')]
 	botPool = {}
 	for i in botConfigs:
@@ -39,11 +82,13 @@ if __name__ == "__main__":
 	for i in botPool:
 		configFilename = i.split('/')[-1].replace('.yaml','')
 		rootLog.info("Starting bot '%s'..",configFilename)
-		botLog = src.log.logger(infoLog=False, infoLogFormat="%(asctime)s %(message)s", defaultFormat="%(asctime)s [%(threadName)s] - %(levelname)s - %(message)s") #TODO: Get these args from somewhere else
-		botObject = src.bot.bot(botPool[i], botLog)
+		botObject = src.bot.bot(botPool[i])
 		t = Thread(target=botObject.run, name=configFilename)
 		threads.append(t)
 		t.start()
 		rootLog.info("Bot '%s' started", configFilename)
 	for i in threads:
 		i.join()
+
+if __name__ == "__main__":
+	run()
