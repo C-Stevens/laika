14f860e4930a230f3c1b22bb7ffd3ec3799bc02b
Colin Stevens <mail@colinjstevens.com>
2015-07-16 19:40:32 -0500

    Push to v1.1

--- _version.py
+++ _version.py
@@ -1,1 +1,13 @@
 __version__ = "Laika 1.0"
+'''
+Changelog
+---------
+Version 1.0
+	* Initial full version release
+Version 1.1
+	* Removed "wait for mask" functionality.
+	* Added support for multiple IRC logging locations for bots.
+	* Fixed configuration values from "Dir" to "Path" or vise versa to improve clarity.
+	* Made command thread pool size user-configurable.
+	* Added runningcommands and flushcommands commands.
+'''
