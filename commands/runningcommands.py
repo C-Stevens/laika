a7fa9d469b43ca626c52aff5727b969456218faf
Colin Stevens <mail@colinjstevens.com>
2015-07-17 11:44:12 -0500

    No longer pluralizes 'command' when only 1 is running

--- commands/runningcommands.py
+++ commands/runningcommands.py
@@ -2,7 +2,7 @@ from src.argument import *
 import src.format
 
 def run(self, **kwargs):
-	self.socket.notice(self.commandData.nick, "You (%s) have %s commands running (including this one) out of a total thread pool size of %s"%(src.format.bold(self.commandData.hostname), src.format.bold(str(self.parent.userThreadCount(self.commandData.hostname))), src.format.bold(str(self.parent.maxUserThreads))))
+	self.socket.notice(self.commandData.nick, "You (%s) have %s command%s running (including this one) out of a total thread pool size of %s"%(src.format.bold(self.commandData.hostname), src.format.bold(str(self.parent.userThreadCount(self.commandData.hostname))), "s" if self.parent.userThreadCount(self.commandData.hostname) > 1 else "", src.format.bold(str(self.parent.maxUserThreads))))
 
 config = {
 	'name' : 'Running Threads',
